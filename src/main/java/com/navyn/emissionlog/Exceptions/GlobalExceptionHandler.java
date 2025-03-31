895641331a28b98edd0006034c7b3a8f672495f6
Navyn27 <phoenixnavyn7@gmail.com>
2025-03-31 15:12:53 +0200

    feat:Exception handling

--- src/main/java/com/navyn/emissionlog/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/navyn/emissionlog/Exceptions/GlobalExceptionHandler.java
@@ -1,2 +1,117 @@
-package com.navyn.emissionlog.Exceptions;public class GlobalExceptionHandler {
+package com.navyn.emissionlog.Exceptions;
+
+import com.navyn.emissionlog.Payload.Responses.ApiResponse;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ApiResponse> handleResponseStatusException(ResponseStatusException ex) {
+        return new ResponseEntity<>(ApiResponse.error(ex.getReason()), ex.getStatusCode());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse> handleValidationException(MethodArgumentNotValidException ex) {
+        // Collect all field errors
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(this::formatFieldError)
+                .collect(Collectors.toList());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.validationError( errors.get(0), errors));
+    }
+
+    private String formatFieldError(FieldError fieldError) {
+        return String.format( "%s",
+                // fieldError.getField(),
+                fieldError.getDefaultMessage());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ApiResponse> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error("Operation failed. File couldn't be deleted or reused"));
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ApiResponse> handleMaxSizeException(MaxUploadSizeExceededException exc) {
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE)
+                .body(ApiResponse.error("File size exceeds the maximum allowed size of 10MB"));
+    }
+
+    @ExceptionHandler(HttpClientErrorException.Unauthorized.class)
+    public ResponseEntity<ApiResponse> handleUnauthorizedException(HttpClientErrorException.Unauthorized e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(ApiResponse.error("Unauthorized"));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(ex.getMessage()));
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<ApiResponse> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(ex.getMessage()));
+    }
+
+    @ExceptionHandler(UnmatchingPasswordsException.class)
+    public ResponseEntity<ApiResponse> handleUnmatchingPasswordsException(UnmatchingPasswordsException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(ex.getMessage()));
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ApiResponse> handleUsernameNotFoundException(UsernameNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ApiResponse.error(ex.getMessage()));
+    }
+
+    @ExceptionHandler(InvalidTokenException.class)
+    public ResponseEntity<ApiResponse> handleInvalidTokenException(InvalidTokenException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(ex.getMessage()));
+    }
+
+    @ExceptionHandler(NoSuchFieldException.class)
+    public ResponseEntity<ApiResponse> handleNoSuchFieldException(NoSuchFieldException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(ex.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse> handleGenericException(Exception ex) {
+        ex.printStackTrace(); // For debugging purposes
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error("An unexpected error occurred"));
+    }
 }
+
+
--- src/main/java/com/navyn/emissionlog/Exceptions/InvalidTokenException.java
+++ src/main/java/com/navyn/emissionlog/Exceptions/InvalidTokenException.java
@@ -1,2 +1,17 @@
-package com.navyn.emissionlog.Exceptions;public class InvalidTokenException {
+package com.navyn.emissionlog.Exceptions;
+
+public class InvalidTokenException extends Exception{
+    Exception exception = new Exception("Invalid token");
+    public InvalidTokenException() {
+        super();
+    }
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+    public InvalidTokenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public InvalidTokenException(Throwable cause) {
+        super(cause);
+    }
 }
