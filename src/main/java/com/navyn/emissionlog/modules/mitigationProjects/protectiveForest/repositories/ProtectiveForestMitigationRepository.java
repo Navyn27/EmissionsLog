2e71b2370544629ea8a9b99d8c2d8931d010057c
Ishimwe Sugira Yvan <68667907+Navyn27@users.noreply.github.com>
2025-10-28 01:19:05 -0500

    feat: Protective forest mitigation project

---
+++ src/main/java/com/navyn/emissionlog/modules/mitigationProjects/protectiveForest/repositories/ProtectiveForestMitigationRepository.java
@@ -0,0 +1,21 @@
+package com.navyn.emissionlog.modules.mitigationProjects.protectiveForest.repositories;
+
+import com.navyn.emissionlog.Enums.Mitigation.ProtectiveForestCategory;
+import com.navyn.emissionlog.modules.mitigationProjects.protectiveForest.models.ProtectiveForestMitigation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ProtectiveForestMitigationRepository 
+        extends JpaRepository<ProtectiveForestMitigation, UUID>,
+                JpaSpecificationExecutor<ProtectiveForestMitigation> {
+    
+    Optional<ProtectiveForestMitigation> findByYearAndCategory(
+        Integer year, 
+        ProtectiveForestCategory category
+    );
+}
